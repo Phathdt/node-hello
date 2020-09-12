76cbfdbf2dd52176e845a02ee341902e5ac0d67d
Phathdt <phathdt379@gmail.com>
2020-09-12 15:01:32 +0700

    test

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
 
-echo "****************************"
-echo "****************************"
-echo "** Building Docker Image ***"
-echo "****************************"
-echo "****************************"
-
-docker build -t ${DOCKER_IMAGE}:${BUILD_ID} .
