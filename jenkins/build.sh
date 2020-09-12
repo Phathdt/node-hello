a6d9fc76c7fc634e104f1cec0afa7f90ef9e944b
Phathdt <phathdt379@gmail.com>
2020-09-12 16:24:07 +0700

    add tag image

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -5,3 +5,4 @@ echo "** Building Docker Image ***"
 echo "****************************"
 
 docker build -t ${DOCKER_IMAGE}:${BUILD_ID} .
+docker tag ${DOCKER_IMAGE}:${BUILD_ID} ${ECR_REPO}:${BUILD_ID}
