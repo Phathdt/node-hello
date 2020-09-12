34729e73bf4b111ff9ecd7940d7b08d06c574294
Phathdt <phathdt379@gmail.com>
2020-09-12 15:06:26 +0700

    update build file

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "****************************"
-echo "****************************"
 echo "** Building Docker Image ***"
-echo "****************************"
 echo "****************************"
 
 docker build -t ${DOCKER_IMAGE}:${BUILD_ID} .
