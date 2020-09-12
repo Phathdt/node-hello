53115c166c6908cd91d98f6c877a43d58a709fbf
Phathdt <phathdt379@gmail.com>
2020-09-12 14:36:26 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+const applicationVersion = process.env.APPLICATION_VERSION;
+const environment = process.env.ENVIROMENT;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = `Hello World! ${applicationVersion} - ${environment}`;
   res.end(msg);
 });
 
