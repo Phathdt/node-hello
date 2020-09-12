a2eef50776edd658d55e7c4b5c3ba3a9c3d51a4c
Phathdt <phathdt379@gmail.com>
2020-09-12 16:35:25 +0700

    update push.sh

--- jenkins/push.sh
+++ jenkins/push.sh
@@ -4,6 +4,7 @@ export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
 export AWS_DEFAULT_OUTPUT=${AWS_DEFAULT_OUTPUT}
 
-aws --region ap-southeast-1 ecr get-login-password     | docker login         --password-stdin         --username AWS         "${ECR}"
+ECR_LOGIN_STRING=`aws ecr get-login --region ${AWS_DEFAULT_REGION} --no-include-email`
 
+eval ${ECR_LOGIN_STRING}
 docker push ${ECR_REPO}:${BUILD_ID}
